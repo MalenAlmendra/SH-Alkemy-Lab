3bb312482e757e6057e44521af4142e33e3da318
Malen Almendra <almendramalen@gmail.com>
2021-07-29 19:36:58 -0300

    New Features in  SearchHero

--- src/pages/SearchHero/SearchHero.jsx
+++ src/pages/SearchHero/SearchHero.jsx
@@ -4,10 +4,16 @@ import HeroCard from "../../components/HeroCard/HeroCard";
 import Navbar from "../../components/Navbar/Navbar";
 const url = "https://superheroapi.com/api.php/2880712572242850/search";
 
-const SearchHero = ({team, takeSelectedHero}) => {
+const SearchHero = ({
+  team,
+  takeSelectedHero,
+  quantityGoodHeroes,
+  quantityBadHeroes,
+}) => {
   const [heroes, setHeroes] = useState([]);
   const [heroSearched, setHeroSearched] = useState("");
- 
+  const [isShowAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
 
   const getHero = () => {
     axios
@@ -36,21 +42,37 @@ const SearchHero = ({team, takeSelectedHero}) => {
     getHero();
   };
 
-  const selectHeroes=(id)=>{
-    console.log(id)
-    if(team.length<6){
-      heroes.forEach(hero=>{
-        if(hero.id===id){
-          takeSelectedHero(hero)
+  const selectHeroes = (id) => {
+    console.log(id);
+    if (team.length < 6) {
+      heroes.forEach((hero) => {
+
+        if (quantityGoodHeroes === 3 && hero.biography.alignment === "good") {
+          setShowAlert(true);
+          setAlertMessage("Cupos de Superheroes Llenos");
+          setTimeout(() => {
+            setShowAlert(false);
+          }, 2300);
+        } else if (
+          quantityBadHeroes === 3 &&
+          hero.biography.alignment === "bad"
+        ) {
+          setShowAlert(true);
+          setAlertMessage("Cupos de Supervillanos Llenos");
+          setTimeout(() => {
+            setShowAlert(false);
+          }, 2300);
+        } else if (hero.id === id) {
+          takeSelectedHero(hero);
         }
-      })
+      });
     }
-  }
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <div className="row">
-      <Navbar />
+        <Navbar />
       </div>
       <div className="row">
         <div className="col-3">
@@ -76,16 +98,41 @@ const SearchHero = ({team, takeSelectedHero}) => {
       <div className="row mt-3">
         {heroes === undefined || heroes.length === 0 ? (
           <div className="">
-            <h1 className="Display">🐱‍👤 No hay resultados disponibles 🐱‍👤</h1>
+            <h1 className="Display">
+              🐱‍👤 No hay resultados disponibles 🐱‍👤
+            </h1>
             <p>Por favor, intente de nuevo con otra palabra</p>
           </div>
         ) : (
           heroes.map((hero) => (
             <div className="col-3">
-              <HeroCard key={hero.id} id={hero.id} heroObject={hero} selectHeroes={selectHeroes} />
+              <HeroCard
+                key={hero.id}
+                id={hero.id}
+                heroObject={hero}
+                selectHeroes={selectHeroes}
+              />
             </div>
           ))
         )}
+      </div>
+      <div className="row">
+        <div className="col-4">
+          <div
+            class={`alert alert-warning alert-dismissible fade position-fixed bottom-0 end-0 ${
+              isShowAlert ? "show" : ""
+            }`}
+            role="alert"
+          >
+            {alertMessage}
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="alert"
+              aria-label="Close"
+            ></button>
+          </div>
+        </div>
       </div>
     </div>
   );
